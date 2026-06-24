badfacf41a0cd3e11f041c6c2463f609e47246a7
Keny R C Noleto <kenyrcnoleto@gmail.com>
2026-06-24 13:31:53 -0300

    insonia api routes extport

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
+
 abstract class Controller
 {
-    //
+    public function __invoke()
+    {
+        //Garantir que os endpoints estejam corretos e os parametros estejam corretos
+        //Garantir que a API (codigo) esteja funcionando corretamente
+
+        $result = Http::get('https://api.github.com');
+
+        dd($result->json());
+    }
 }
