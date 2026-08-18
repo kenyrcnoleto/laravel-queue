a221bd3b00ffc5f7d1557f73c0ef3a91da96bbab
Keny R C Noleto <kenyrcnoleto@gmail.com>
2026-08-18 18:19:54 -0300

    crating  ComputedPullRequestsCount job

--- app/Services/Github/Console/Commands/PullRequestOneSync.php
+++ app/Services/Github/Console/Commands/PullRequestOneSync.php
@@ -30,10 +30,35 @@ class PullRequestOneSync extends Command
     $repositoryFullName = $this->argument('repositoryFullName');
         Bus::batch([
             new  \App\Services\Github\Jobs\PullRequestOneSync($repositoryFullName),
-            new \App\Services\Github\Jobs\FailedJob(),
+            // new \App\Services\Github\Jobs\FailedJob(),
+
+            //Sync todos os dados do repositório
+            //Compute data
+            //Computed tables
+            //total users = 10
+            // Saber quantos PRs tem ao todo na aplicação
+
+            // Cout dos Prs - muito lenta e complexa, então não é possível fazer isso em tempo real, então vamos criar uma tabela para armazenar o total de PRs e atualizar essa tabela a cada sync
             ])
             ->then(function() {
-                info('All jobs completed successfully.');
+
+                Bus::batch([
+                    new \App\Services\Github\Jobs\ComputedPullRequestsCount(),
+
+                ])
+                 ->then(function() {
+                     info('All computed data jobs completed successfully.');
+                })
+                ->name('github computed data sync')
+                ->allowFailures()
+                ->dispatch();
+
+                // new ComputedPullRequestsCount::dispatch();
+                // new ComputedUsersCount::dispatch();
+                // new ComputedCommitsCount::dispatch();
+                // new ComputedCollaboratorsCount::dispatch();
+
+                // info('All jobs completed successfully.');
             })
             ->name('github repository sync: ' . $repositoryFullName)
             ->allowFailures()
---
+++ app/Services/Github/Jobs/ComputedPullRequestsCount.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Services\Github\Jobs;
+
+use App\Models\PullRequest;
+use App\Services\Github\PullRequestService;
+use Illuminate\Bus\Batchable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\Middleware\SkipIfBatchCancelled;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Http;
+
+class ComputedPullRequestsCount implements ShouldQueue
+{
+    use Queueable;
+    use Batchable;
+
+
+
+    public function __construct(public string $repositoryFullName, public ?int $page = 1)
+    {
+        //
+    }
+
+    public function middleware(): array
+    {
+        return [
+            new SkipIfBatchCancelled
+        ];
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $pullRequests = (new PullRequestService())->getPullRequests($this->repositoryFullName, $this->page);
+
+
+        if(empty($pullRequests) || !is_array($pullRequests)) {
+            return;
+        }
+
+        $jobs = [];
+
+
+       foreach ($pullRequests as $pullRequest) {
+            //Salvar cada pull request em um job separado, para evitar sobrecarga de memória e processamento.
+            $jobs[] = new PullRequestSync($this->repositoryFullName, $pullRequest['number']);
+            // $this->batch()->add([new PullRequestSync($this->repositoryFullName, $pullRequgit sest['number'])]);
+            // PullRequestSync::dispatch($this->repositoryFullName, $pullRequest['number']);
+       }
+
+       $this->batch()->add($jobs);
+
+       $nextPage = $this->page + 1;
+
+       // Chamar o próximo job para a próxima página de pull requests.
+       $this->batch()->add(new PullRequestOneSync($this->repositoryFullName, $nextPage));
+
+    }
+
+
+    /*public function sync(int $page = 1): void
+    {
+        $pullRequestsResponse = Http::get('https://api.github.com/repos/laravel/laravel/pulls?state=all&page=' . $page);
+
+        $pullRequests = $pullRequestsResponse->json();
+
+        if(empty($pullRequests) || !is_array($pullRequests)) {
+            return;
+        }
+
+        foreach ($pullRequests as $pullRequest) {
+
+            dd($pullRequest);
+        }
+
+        foreach ($pullRequests as $pullRequest) {
+            PullRequest::create(
+                [
+                    'api_id' => $pullRequest['id'],
+                    'api_number' => $pullRequest['number'],
+                    'state' => $pullRequest['state'],
+                    'title' => $pullRequest['title'],
+                    'api_created_at' => Carbon::parse($pullRequest['created_at'])->format('Y-m-d H:i:s'),
+                    'api_updated_at' => Carbon::parse($pullRequest['updated_at'])->format('Y-m-d H:i:s'),
+                    'api_closed_at' => Carbon::parse($pullRequest['closed_at'])->format('Y-m-d H:i:s'),
+                    'api_merged_at' => Carbon::parse($pullRequest['merged_at'])->format('Y-m-d H:i:s'),
+                ]
+            );
+       }
+
+       $this->sync($page + 1);
+
+    }*/
+}
